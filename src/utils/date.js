3446821ce5ec12d6b3211deeeeda3ac7ee200e44
MaetDol <rndka9@gmail.com>
2020-10-06 16:28:28 +0900

    Revert by hand #da90a4c6b04645cd31a0188cd2199bfd036679b3

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,42 +1,48 @@
 const { format } = require('date-fns');
 
+function getKSTDate() {
+  return new Date();
+}
+
+function getYesterday() {
+  const yesterday = getKSTDate();
+  return yesterday.setDate( yesterday.getDate() - 1 );
+}
+
+function isWrittenToday( date ) {
+  return toLocaleDateString( getKSTDate() ) === toLocaleDateString( date );
+}
+
+function isWrittenYesterday( date ) {
+  return toLocaleDateString( getYesterday() ) === toLocaleDateString( date );
+}
+
+function toLocaleDateString( d ) { 
+  return format( d, 'yyyy-MM-dd', {timezone: 'Asia/Seoul'});
+}
+
+function toLocaleTimeString( d ) {
+  return format( d, 'kk:mm:ss', {timezone: 'Asia/Seou'});
+}
+
+function toLocaleString( d ) {
+  return {
+    date: toLocaleDateString( d ),
+    time: toLocaleTimeString( d ),
+  };
+}
+
+function formattingDate( date, y, m, d='' ) {
+  return format( date, `yyyy${y}MM${m}dd${d}` );
+}
+
 module.exports = {
-
-  function getKSTDate() {
-    return new Date();
-  },
-
-  function getYesterday() {
-    const yesterday = getKSTDate();
-    return yesterday.setDate( yesterday.getDate() - 1 );
-  },
-
-  function isWrittenToday( date ) {
-    return toLocaleDateString( getKSTDate() ) === toLocaleDateString( date );
-  },
-
-  function isWrittenYesterday( date ) {
-    return toLocaleDateString( getYesterday() ) === toLocaleDateString( date );
-  },
-
-  function toLocaleDateString( d ) { 
-    return format( d, 'yyyy-MM-dd', {timezone: 'Asia/Seoul'});
-  },
-
-  function toLocaleTimeString( d ) {
-    return format( d, 'kk:mm:ss', {timezone: 'Asia/Seou'});
-  },
-
-  function toLocaleString( d ) {
-    return {
-      date: toLocaleDateString( d ),
-      time: toLocaleTimeString( d ),
-    };
-  },
-
-  function formattingDate( date, y, m, d='' ) {
-    return format( date, `yyyy${y}MM${m}dd${d}` );
-  },
-
+  getKSTDate,
+  getYesterday,
+  isWrittenToday,
+  isWrittenYesterday,
+  toLocaleDateString,
+  toLocaleTimeString,
+  toLocaleString,
+  formattingDate,
 };
-
