621937e4cd5150b8ce0e501676406d51f957c444
MaetDol <rndka9@gmail.com>
2020-10-08 13:56:40 +0900

    Add signing secret verify
    Filtering not valid post requests.
    Redeem logger message.

--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
 const http = require('http');
 const router = require('./router');
 const logger = require('./utils/logger');
+const verify = require('./utils/verifyRequest');
 
+const CORS_SLACK = 'https://slack.com';
 http.createServer(( req, res ) => {
 
   res.setHeader('Access-Control-Allow-Origin', '*');
 
-  logger.info(`Request ${req.url}, ${req.method}`);
   const isSupportedRequest = req.method === 'POST' && req.url === '/';
   if( !isSupportedRequest ) {
-    console.log('Not supported request');
+    logger.log('Not supported request');
     res.writeHead(405);
     res.end();
     return;
@@ -18,12 +19,23 @@ http.createServer(( req, res ) => {
   let body = '';
   req.on('data', ch => body += ch );
   req.on('end', () => {
-    console.log('POST: ', JSON.parse( body ));
-    const responseBody = router.route( body );
-    res.writeHead( 200 );
-    res.end( body );
+    logger.info(`Request ${req.url}, ${req.method}\n\tFrom ${res.socket.remoteAddress}\n\tHeader ${JSON.stringify(req.headers)}\n\tBody ${body}`);
+    let responseBody = '';
+    try {
+      const timestamp = req.headers['x-slack-request-timestamp'];
+      const isInvalidRequest = verify( timestamp, body ) !== req.headers['x-slack-signature'];  
+      if( isInvalidRequest ) throw `Invalid request`;
+
+      responseBody = router.route( body );
+      res.writeHead( 200 );
+    } catch(e) {
+      logger.error(`Raised error to root: ${e}`);
+      res.writeHead( 500 );
+    } finally {
+      res.end( responseBody );
+    }
   });
 
 }).listen( 8079 );
 
-console.log('Server start');
+logger.info('Server start');
